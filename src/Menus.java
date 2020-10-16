eb591e0b20ef7f4ad52ff6f6b32dd2a3d8287df7
T.Gadiffet <theo.gadiffet@leroymerlin.fr>
2020-10-16 12:01:29 +0200

    add(Swing):Add new interface

--- src/Menus.java
+++ src/Menus.java
@@ -2,19 +2,42 @@ import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Menus {
     private static Scanner scan = new Scanner(System.in);
     private static DecimalFormat df = new DecimalFormat("0.00");
 
+
     // MENU PRINCIPAL DE L'APPLICATION
     public static String MainMenu() {
-        System.out.println("*-----------------------------*");
-        System.out.println("* 1. Commander une Pizza      *");
-        System.out.println("* 2. Payer vos Pizza          *");
-        System.out.println("*-----------------------------*");
-        System.out.println("* 0. Arret du systeme         *");
-        System.out.println("*-----------------------------*");
-        System.out.print("* -> Option : ");
+        JFrame frame = new JFrame("Arti'Pizza");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(720, 720);
+        frame.setVisible(true);
+
+        JLabel label = new JLabel("Bienvenue !");
+        frame.add(label);
+
+        //On définie le panel
+        JPanel panel = new JPanel();
+
+        JButton btn1 = new JButton("Commander une Pizza");
+        JButton btn2 = new JButton("Payer vos Pizza ");      
+        JButton btn3 = new JButton("Arret du systeme ");   
+
+        // Ajouter les boutons au frame
+        panel.add(btn1); 
+        panel.add(btn2);
+        panel.add(btn3); 
+         
+        // Ajouter label et panel au frame
+        frame.add(label);
+        frame.add(panel);
+
         String userOpt = scan.next();
         // On retourne le choix utilisateur
         return userOpt;
